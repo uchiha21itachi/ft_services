d1217e0ccf94c57fe84890c3ee77c7d44f861210
Yash Sharma <yash221b@gmail.com>
2021-04-12 18:07:49 +0200

    fixing nginx ssh congiguration

--- containers/nginx/nginx_srcs/start.sh
+++ containers/nginx/nginx_srcs/start.sh
@@ -1,28 +1,38 @@
 echo "starting ......"
 
 echo 'root:password' | chpasswd
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-
-
-# Creating SSL keys
-# openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt
-
-# Nginx setup
-# adduser -D -g 'www' www
-# chown -R www:www /var/lib/nginx
-# chown -R www:www /www
+echo 'Password changed succesfully\n'
 
 # Nginx & SSH starting
-openrc
+echo "setting up openrc"
+openrc &> /dev/null
 touch /run/openrc/softlevel
-service nginx start
-service sshd start
 
+echo "Done openrc"
+# rc-service nginx start
+
+echo "addong user"
+adduser "/www" "$SSH_USER"
+echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+addgroup "$SSH_USER" www
+
+echo "addong user done"
+
+
+# rc-service sshd start
+echo "starting sshd"
+/etc/init.d/sshd start
+
+echo "done sshd"
 tail -F /dev/null
+# echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
-echo "Nginx started.............."
-echo "SSHD started.............."
+# echo "Nginx started.............."
+# echo "SSHD started.............."
 sleep 10
+nginx -t || exit 1
+
+
 
 
 
@@ -31,16 +41,16 @@ sleep 10
 
 # openrc
 # touch /run/openrc/softlevel
-#/etc/init.d/sshd start
 
-nginx -t || exit 1
+
 #nginx -g 'daemon off'
-
-#adduser "/www" "$SSH_USER"
-#echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-#addgroup "$SSH_USER" www
-
 
 #echo "hello" 
 #/usr/sbin/sshd
 #nginx -g "daemon off;"
+
+
+# Nginx setup
+# adduser -D -g 'www' www
+# chown -R www:www /var/lib/nginx
+# chown -R www:www /www
