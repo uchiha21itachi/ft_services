532a109cf8a5434a3c1240a560202917782bf164
yash sharma <yash221b@gmail.com>
2021-04-13 14:16:07 +0200

    fixing sshd for nginx

--- containers/nginx/nginx_srcs/start.sh
+++ containers/nginx/nginx_srcs/start.sh
@@ -1,56 +1,40 @@
 echo "starting ......"
 
 echo 'root:password' | chpasswd
-echo 'Password changed succesfully\n'
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
-# Nginx & SSH starting
-echo "setting up openrc"
-openrc &> /dev/null
+openrc
+echo "openrc done"
 touch /run/openrc/softlevel
+echo "touch done"
+service nginx start
+echo "nginx start done"
+service sshd start
+echo "sshd start done"
 
-echo "Done openrc"
-# rc-service nginx start
-
-echo "addong user"
-adduser "/www" "$SSH_USER"
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-addgroup "$SSH_USER" www
-
-echo "addong user done"
-
-
-# rc-service sshd start
-echo "starting sshd"
-/etc/init.d/sshd start
-
-echo "done sshd"
 tail -F /dev/null
-# echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
-# echo "Nginx started.............."
-# echo "SSHD started.............."
-sleep 10
-nginx -t || exit 1
+echo "Nginx started.............."
+echo "SSHD started.............."
+sleep 5
 
-
+nginx -t 
 
 
-
-
-
-
-# openrc
+# # Nginx & SSH starting
+# echo "setting up openrc"
+# openrc &> /dev/null
 # touch /run/openrc/softlevel
 
+# echo "Done openrc"
+# nginx -t || exit 1
+# # rc-service nginx start
 
-#nginx -g 'daemon off'
-
-#echo "hello" 
-#/usr/sbin/sshd
-#nginx -g "daemon off;"
+# # /etc/init.d/sshd start
 
 
-# Nginx setup
-# adduser -D -g 'www' www
-# chown -R www:www /var/lib/nginx
-# chown -R www:www /www
+
+# #nginx -g 'daemon off'
+
+# #echo "hello" 
+# # /usr/sbin/sshd
