c75425de9f30438f65b8b87248499044ae3f0be1
user42 <yash221b@gmail.com>
2021-04-14 16:42:54 +0200

    small changes

--- creator.sh
+++ creator.sh
@@ -1,19 +1,10 @@
-
-echo "Starting to delete the config...."
-kubectl delete -f yaml/metallb.yaml
-kubectl delete -f yaml/wordpress.yaml
-kubectl delete -f yaml/phpmyadmin.yaml
-kubectl delete -f yaml/nginx.yaml
-# kubectl delete -f yaml/mysql.yaml
-sleep 1
-echo "updating docker file......"
+echo "Creating docker file......"
 eval $(minikube -p minikube docker-env)
 docker build -t ft-mysql containers/mysql/.
 docker build -t ft-nginx containers/nginx/.
 docker build -t ft-phpmyadmin containers/phpmyadmin/.
 docker build -t ft-wordpress containers/wordpress/.
 echo "Creating deployment and service......"
-kubectl apply -f yaml/metallb.yaml
 kubectl apply -f yaml/mysql.yaml
 kubectl apply -f yaml/nginx.yaml
 kubectl apply -f yaml/phpmyadmin.yaml
