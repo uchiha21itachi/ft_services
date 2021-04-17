d4381f5a7b5a8e825031e8d84635a81cee48f045
user42 <yash221b@gmail.com>
2021-04-17 17:27:54 +0200

    small changes

--- containers/nginx/nginx_srcs/start.sh
+++ containers/nginx/nginx_srcs/start.sh
@@ -1,22 +1,22 @@
 
-# echo "starting ......"
-# echo "Starting sshd in /usr/sbin/sshd ......."
-# /usr/sbin/sshd
-# echo "sshd started"
-# echo "Starting nginx ........"
-# nginx -g "daemon off;"
-# nginx -t 
-# echo "all done"
-# tail -F /dev/null
+echo "starting ......"
+echo "Starting sshd in /usr/sbin/sshd ......."
+/usr/sbin/sshd
+echo "sshd started"
+echo "Starting nginx ........"
+nginx -g "daemon off;"
+nginx -t 
+echo "all done"
+tail -F /dev/null
 
 
-echo "Service 'All': Status"
+# echo "Service 'All': Status"
 
-rc-status -a
+# rc-status -a
 
-echo "Service 'RSyslog': Starting ..."
+# echo "Service 'RSyslog': Starting ..."
 
-rc-service rsyslog start
+# rc-service rsyslog start
 
 
 
@@ -28,12 +28,12 @@ rc-service rsyslog start
 # echo "openrc done"
 # touch /run/openrc/softlevel
 # echo 'root:password' | chpasswd
-rc-service nginx start
-echo "nginx start done"
-rc-service sshd start
-echo "sshd start done"
+# rc-service nginx start
+# echo "nginx start done"
+# rc-service sshd start
+# echo "sshd start done"
 
-# 
+# # 
 # echo "Nginx started.............."
 # echo "SSHD started.............."
 # sleep 5
